240ab0d6c4afa9d74ae00bfb68bf648d9e03361a
amaankhalid52 <ak01654@surrey.ac.uk>
2020-10-27 12:04:11 +0000

    Add a test for the contact action of the home controller

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,9 +4,11 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select 'title', 'Car Park Space Bookings'
-    assert_select 'h1', 'Home Page'
-    assert_select 'p', 'Welcome to the home page!'
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
   end
 
 end
