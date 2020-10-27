65dc3e6e9d66d1a89a6fcc20043cba1515ab81e0
amaankhalid52 <ak01654@surrey.ac.uk>
2020-10-27 09:47:24 +0000

    Adjust home controller routes for more sensible URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Routes for home.
-  get 'home/home'
-  get 'home/contact'
+  get 'contact', to: 'home#contact'
 
   # Make home the root of the site.
   root 'home#home'
