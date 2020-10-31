51c0a6c14f0657964dd74204a2ba491f74932e77
amaankhalid52 <ak01654@surrey.ac.uk>
2020-10-31 10:25:49 +0000

    Modify content of contact view and make tests for home controller actions more comprehensive

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,11 +4,31 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
+
+
+    assert_template layout: 'application'
+    assert_template partial: '_footer', count: 1
+    assert_template partial: '_header', count: 1
+
+
+    assert_select 'title', 'Car Park Space Bookings'
+    assert_select 'h1', 'Home Page'
+    assert_select 'p', 'Welcome to the home page!'
   end
 
   test "should get contact" do
     get contact_url
     assert_response :success
+
+
+    assert_template layout: 'application'
+    assert_template partial: '_footer', count: 1
+    assert_template partial: '_header', count: 1
+
+
+    assert_select 'title', 'Car Park Space Bookings'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to get in touch with us.'
   end
 
 end
