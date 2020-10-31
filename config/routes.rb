4f0fc3731c4227269221d8aa685d4bc678af52e1
amaankhalid52 <ak01654@surrey.ac.uk>
2020-10-31 11:04:09 +0000

    Added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Routes for home.
   get 'contact', to: 'home#contact'
+  post 'request_contact', to: 'home#request_contact'
 
   # Make home the root of the site.
   root 'home#home'
