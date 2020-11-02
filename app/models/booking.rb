b564dc8a8bafeff54f0757fe2d7eeda3e283184c
amaankhalid52 <ak01654@surrey.ac.uk>
2020-11-02 18:19:44 +0000

    Add some appropriate validations to models for tables that will be written to

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,20 @@ class Booking < ApplicationRecord
   belongs_to :vehicle
   # One booking will belong to/be of one particular cost type/price.
   belongs_to :cost_type
+
+  # Validations.
+  # DO I NEED VALIDATES_ASSOCIATED FOR VEHICLE? (REMEMBER, NEVER PUT VALIDATES_ASSOCIATED ON BOTH ENDS OF AN ASSOCIATION)
+  # Not sure if I need this validation - I plan on providing a drop-down menu or (perhaps later, a bit more complex) an interactive diagram which will autofill the following fields
+  validates :space, :vehicle, :cost_type, numericality: { only_integer: true }
+  # Need to validate presence of any fields except date? Do I need to validate the presence of the foreign keys and the objects associated with those, or is that done automatically?
+  validates :date, presence: true
+
+  # Prevent a particular space from being booked twice on the same day.
+  # Double-check that the following validation makes sense.
+  validates :space, uniqueness: { scope: :date,
+    message: "A parking space cannot be booked twice for one day." }
+
+    # Double-check that the following validation makes sense.
+  validates :vehicle, uniqueness: { scope: :date,
+    message: "A vehicle cannot be booked twice for one day." }
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,4 +2,13 @@ class Vehicle < ApplicationRecord
   # There may be 0 or more bookings made for a particular vehicle.
   # But these should be removed if the vehicle is unregistered from the site.
   has_many :bookings, dependent: :destroy
+
+  # Validations.
+  # ADD VALIDATION FOR NUMBER PLATE/REGISTRATION NUMBER USING 'FORMAT' HELPER
+  # FOR NUMBER PLATE FORMAT: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/359317/INF104_160914.pdf
+  # SEE IF YOU CAN TAKE OLD NUMBER PLATE FORMAT INTO ACCOUNT I.E. FOR OLDER CARS
+  validates :make, :model, :colour, length: { minimum: 2 }
+  validates :registration_number, :make, :model, presence: true
+  # Ensure the reg. number/number plate has not already been registered on the site.
+  validates :registration_number, uniqueness: { case_sensitive: false } # Is this correct? Made case_sensitive false to prevent the same number plate being added twice if it has different cases.
 end
