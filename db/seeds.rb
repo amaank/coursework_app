e0cd860f454ff62c0ef0045572846e2a6a76a804
amaankhalid52 <ak01654@surrey.ac.uk>
2020-11-04 17:50:06 +0000

    Set up seeds.rb and seed the database with the initial data required by the application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Parking space data.
+
+# Floor 1: 6 rows, 5 columns (30 spaces)
+for i in 1..6 do
+  for j in 1..5 do
+    Space.create(floor: 1, row: i, column: j)
+  end
+end
+
+# Floor 2: 6 rows, 6 columns (36 spaces)
+for i in 1..6 do
+  for j in 1..6 do
+    Space.create(floor: 2, row: i, column: j)
+  end
+end
+
+# Floor 3: 6 rows, 7 columns (42 spaces)
+for i in 1..6 do
+  for j in 1..7 do
+    Space.create(floor: 3, row: i, column: j)
+  end
+end
+
+# Floor 4: 4 rows, 5 columns (20 spaces)
+for i in 1..4 do
+  for j in 1..5 do
+    Space.create(floor: 4, row: i, column: j)
+  end
+end
+
+# Cost types data.
+CostType.create(name: "Basic", price: 7.5, description: "Our basic parking tier provides you with a traditional parking experience, with no additional services included.")
+CostType.create(name: "Standard Valet", price: 12.5, description: "Standard-tier valet takes the stress out of parking your own vehicle, by offering you the services of one of our well-trained staff who will park your car for you, and bring it out to meet you upon your return.")
+CostType.create(name: "Advanced Valet", price: 17.5, description: "For those looking to get more out of their everyday park, advanced-tier valet parking offers an external wash of your car, in addition to the standard-tier valet benefits.")
+CostType.create(name: "Luxury Valet", price: 22.5, description: "Luxury-tier valet offers you the ultimate parking experience, providing internal cleaning of the car, in addition to the advanced-tier valet benefits.")
--- test/system/spaces_test.rb
+++ test/system/spaces_test.rb
@@ -1,9 +1,6 @@
 require "application_system_test_case"
 
 class SpacesTest < ApplicationSystemTestCase
-  setup do
-    @space = spaces(:one)
-  end
 
   test "visiting the index" do
     visit spaces_url
