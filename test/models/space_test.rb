47f6555f1a16a1ab97c0c323e4defc13a2dc1835
amaankhalid52 <ak01654@surrey.ac.uk>
2020-11-08 09:33:52 +0000

    Refactor Space tests to remove duplication

--- test/models/space_test.rb
+++ test/models/space_test.rb
@@ -1,35 +1,40 @@
 require 'test_helper'
 
 class SpaceTest < ActiveSupport::TestCase
-  test "valid space" do
+  def setup
     unused = Space.all.count + 1
-    space = Space.new(floor: unused, row: unused, column: unused)
-    assert space.valid?
+    @space = Space.new(floor: unused, row: unused, column: unused)
+  end
+
+  test "valid space" do
+    assert @space.valid?
   end
 
   test "invalid without floor" do
-    space = Space.new(row: 1, column: 1)
-    refute space.valid?
-    assert_not_nil space.errors[:floor]
+    @space.floor = nil
+    refute @space.valid?
+    assert_not_nil @space.errors[:floor]
   end
 
   test "invalid without row" do
-    space = Space.new(floor: 1, column: 1)
-    refute space.valid?
-    assert_not_nil space.errors[:row]
+    @space.row = nil
+    refute @space.valid?
+    assert_not_nil @space.errors[:row]
   end
 
   test "invalid without column" do
-    space = Space.new(floor: 1, row: 1)
-    refute space.valid?
-    assert_not_nil space.errors[:column]
+    @space.column = nil
+    refute @space.valid?
+    assert_not_nil @space.errors[:column]
   end
 
   test "invalid if duplicate floor, row and column combination" do
     used = Space.first
     assert used.valid?
-    space = Space.new(floor: used.floor, row: used.row, column: used.column)
-    refute space.valid?
-    assert_not_nil space.errors[:index_spaces_on_floor_and_row_and_column]
+    @space.floor = used.floor
+    @space.row = used.row
+    @space.column = used.column
+    refute @space.valid?
+    assert_not_nil @space.errors[:index_spaces_on_floor_and_row_and_column]
   end
 end
