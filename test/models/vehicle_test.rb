46a4f5e2485d48b6664ac0c5b22b647d09b75e86
amaankhalid52 <ak01654@surrey.ac.uk>
2020-11-08 11:28:10 +0000

    Write tests for Vehicle model validations, ensure they all pass

--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class VehicleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @vehicle = Vehicle.new(registration_number: "XX00 XXX", make: "Ford", model: "Focus", colour: "Blue")
+  end
+
+  test "valid vehicle" do
+    assert @vehicle.valid?
+  end
+
+  test "invalid if registration number not correctly formatted" do
+    @vehicle.registration_number += "X"
+    refute @vehicle.valid?
+    assert_not_nil @vehicle.errors[:registration_number]
+  end
+
+  test "invalid without registration number" do
+    @vehicle.registration_number = nil
+    refute @vehicle.valid?
+    assert_not_nil @vehicle.errors[:registration_number]
+  end
+
+  test "invalid without make" do
+    @vehicle.make = nil
+    refute @vehicle.valid?
+    assert_not_nil @vehicle.errors[:make]
+  end
+
+  test "invalid without model" do
+    @vehicle.model = nil
+    refute @vehicle.valid?
+    assert_not_nil @vehicle.errors[:model]
+  end
+
+  test "invalid if make length less than 2" do
+    @vehicle.make = "a"
+    refute @vehicle.valid?
+    assert_not_nil @vehicle.errors[:make]
+  end
+
+  test "invalid if model length less than 2" do
+    @vehicle.model = "a"
+    refute @vehicle.valid?
+    assert_not_nil @vehicle.errors[:model]
+  end
+
+  test "invalid if duplicate registration number, case-insensitive" do
+    @vehicle.registration_number = Vehicle.first.registration_number.swapcase
+    refute @vehicle.valid?
+    assert_not_nil @vehicle.errors[:registration_number]
+  end
 end
