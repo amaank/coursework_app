5f3d62cf371d9d1274e275864a75a58528fbf76e
amaankhalid52 <ak01654@surrey.ac.uk>
2020-11-27 16:26:46 +0000

    Refactor client-side validation for mailer and new vehicle form to provide more guidance to the user, and prevent mailer redirect on invalid data

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -8,15 +8,22 @@ document.addEventListener("turbolinks:load", function() {
       // Ensure telephone is a string of digits only.
       telephone: {digits: true, required: true},
       // Ensure email has appropriate format.
-      email: {email: true}
+      email: {email: true, required: true}
     },
     // Custom error messages.
     messages: {
+      name: {
+        required: "Please enter a name."
+      },
       telephone: {
-        digits: "This must be a single, unseparated number"
+        // Provide an example of valid input, if user has entered a telephone but it is invalid.
+        digits: "Telephone number must be a single, unseparated number, for example: 07123456789",
+        required: "Please enter a telephone number."
       },
       email: {
-        email: "Enter a valid email address"
+        // Provide an example of valid input, if user has entered an email but it is invalid.
+        email: "Email address must be of the usual format, for example: joebloggs@example.com",
+        required: "Please enter an email address."
       },
     }
   });
--- app/assets/javascripts/vehicles.js
+++ app/assets/javascripts/vehicles.js
@@ -16,6 +16,22 @@ document.addEventListener("turbolinks:load", function() {
         required: true,
         minlength: 2
       }
+    },
+    // Custom error messages.
+    messages: {
+      "vehicle[registration_number]": {
+        required: "Please enter a registration number."
+      },
+      "vehicle[make]": {
+        // Provide an example in-case user is unsure of what should go in this field.
+        required: "Please enter a make - this is the manufacturer of your vehicle, for example: Audi",
+        minlength: "The make should be at least 2 characters in length."
+      },
+      "vehicle[model]": {
+        // Identify this as a required field, and provide an explanation for why it is needed.
+        required: "Please enter the model of your vehicle. This helps identify your car in-case of theft or criminal damage.",
+        minlength: "The model should be at least 2 characters in length."
+      }
     }
   });
 
