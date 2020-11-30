64232491d0e86c6d1c5fbc284eedd5fe1cf6dcad
amaankhalid52 <ak01654@surrey.ac.uk>
2020-11-30 17:30:03 +0000

    Tailor mailer to improve aesthetics, and refactor mailer preview and mailer test to account for these changes to the view

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -5,10 +5,11 @@ class ContactMailerTest < ActionMailer::TestCase
   # Test for email being returned by mailer.
   test "should return contact email" do
     mail = ContactMailer.contact_email("amaan@me.com",
-      "Amaan Khalid", "1234567890", @message = "Hello")
+      "Amaan Khalid", "1234567890", @message = I18n.t('mailer.message'))
     # Test content of returned email.
     assert_equal ['info@parkmycars.com'], mail.to
     assert_equal ['info@parkmycars.com'], mail.from
+    assert_equal ['amaan@me.com'], mail.cc
   end
 
 end
--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -4,7 +4,7 @@ class ContactMailerPreview < ActionMailer::Preview
   # Define a preview (test) for mailer view.
   def contact_email
     ContactMailer.contact_email("amaan@me.com",
-      "Amaan Khalid", "1234567890", @message = "Hello")
+      "Amaan Khalid", "1234567890", @message = I18n.t('mailer.message'))
   end
 
 end
