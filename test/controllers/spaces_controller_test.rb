4cfebd47f163ca3b4a3fbf0198f4ba509b710592
amaankhalid52 <ak01654@surrey.ac.uk>
2020-12-07 16:20:54 +0000

    Add further testing for Spaces controller

--- test/controllers/spaces_controller_test.rb
+++ test/controllers/spaces_controller_test.rb
@@ -8,7 +8,7 @@ class SpacesControllerTest < ActionDispatch::IntegrationTest
     sign_in @user
   end
 
-  # Test index action.
+  # Test index action when no date supplied.
   test "should get index for today, if no date provided" do
     # Perform RESTful request without supplying any parameters (no date).
     get spaces_url
@@ -60,12 +60,78 @@ class SpacesControllerTest < ActionDispatch::IntegrationTest
     assert_equal "You need to sign in or sign up before continuing.", flash[:alert]
   end
 
-  test "should get index for today, if invalid date provided" do
+  # Test index action when invalid date supplied.
+  test "should get index for today, if date in past (invalid) provided" do
+    # Perform RESTful request with date supplied within the parameters.
+    # Provide a date which is in the past (invalid).
+    get spaces_url, params: {date: Date.yesterday}
 
+    # Check that the request was performed successfully.
+    assert_response :success
+
+    # Ensure that a @spaces instance variable has been set.
+    assert_not_nil assigns(:spaces)
+
+    # Ensure that a @chosen_date instance variable has been set equal to today's date.
+    assert_equal assigns(:chosen_date), Date.today
+
+    # Test for appropriate flash alert to tell the user that the date was invalid.
+    assert_equal "#{I18n.t('spaces.index.invalid_date.too_early')}", flash.now[:alert]
   end
 
-  test "should get index for chosen date, if valid date provided" do
+  # Test index action when invalid date supplied.
+  test "should get index for today, if date more than 7 days in future (invalid) provided" do
+    # Perform RESTful request with date supplied within the parameters.
+    # Provide a date which is more than 7 days from today (invalid).
+    get spaces_url, params: {date: Date.today + 8.days}
 
+    # Check that the request was performed successfully.
+    assert_response :success
+
+    # Ensure that a @spaces instance variable has been set.
+    assert_not_nil assigns(:spaces)
+
+    # Ensure that a @chosen_date instance variable has been set equal to today's date.
+    assert_equal assigns(:chosen_date), Date.today
+
+    # Test for appropriate flash alert to tell the user that the date was invalid.
+    assert_equal "#{I18n.t('spaces.index.invalid_date.too_late')}", flash.now[:alert]
+  end
+
+  # Test index action when invalid date supplied.
+  test "should get index for today, if date provided cannot be parsed" do
+    # Perform RESTful request with date supplied within the parameters.
+    # Provide a value which is not a date, for the date parameter.
+    get spaces_url, params: {date: "not a date"}
+
+    # Check that the request was performed successfully.
+    assert_response :success
+
+    # Ensure that a @spaces instance variable has been set.
+    assert_not_nil assigns(:spaces)
+
+    # Ensure that a @chosen_date instance variable has been set equal to today's date.
+    assert_equal assigns(:chosen_date), Date.today
+
+    # Test for appropriate flash alert to tell the user that the date was invalid.
+    assert_equal "#{I18n.t('spaces.index.invalid_date.default')}", flash.now[:alert]
+  end
+
+  # Test index action when valid date supplied.
+  test "should get index for chosen date, if valid date provided" do
+    # Perform RESTful request with date supplied within the parameters.
+    # Provide a valid date.
+    valid_date = Date.today + 7.days
+    get spaces_url, params: {date: valid_date}
+
+    # Check that the request was performed successfully.
+    assert_response :success
+
+    # Ensure that a @spaces instance variable has been set.
+    assert_not_nil assigns(:spaces)
+
+    # Ensure that a @chosen_date instance variable has been set equal to the chosen date.
+    assert_equal assigns(:chosen_date), valid_date
   end
 
 end
